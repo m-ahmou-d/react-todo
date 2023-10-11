17f77c1b8948377316c258f7b59de16fab857f76
Mahmoud Elrmly <mahmoud.el912@gmail.com>
2023-10-11 01:50:28 -0600

    Lesson 1-1 updated app.js with ToDo List heading and an array with todo tasks, also added them to the app in list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Complete Assignment',
+    type: 'School Work'
+  },
+  {
+    id: 2,
+    title: 'Go to the Gym',
+    type: 'Personal'
+  },
+  {
+    id: 3,
+    title: 'Work 10 Hours',
+    type: 'Work'
+  },
+  {
+    id: 4,
+    title: 'Do Laundry',
+    type: 'Personal'
+  },
+  {
+    id: 5,
+    title: 'Groceries',
+    type: 'Errands'
+  },
+  {
+    id: 6,
+    title: 'Get an Oil Change',
+    type: 'Errands'
+  },
+]
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      
+    <div>
+      <h1>Todo List</h1>
+      <ul>{todoList.map(function(item) {
+        return <li>{item.id + ': ' + item.title + ' - ' + item.type}</li>
+      })}
+      </ul>
     </div>
   );
 }
